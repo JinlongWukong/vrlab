57f697437344d15c798f26f8a2d6a45292421afa
JinlongWukong <892277894@qq.com>
2018-10-11 23:36:17 +0800

    add support linux server

--- vrlab/utils.py
+++ vrlab/utils.py
@@ -2,11 +2,14 @@ from vriosxrv import iosxrv
 from vrcsr1000v import csr1000v
 from vrasav import asav
 from vrnxos import n9kv
+from vrlinux import linux
+
 vrouters = {
     "IOS XRv" : iosxrv,
     "CSR1000v" : csr1000v,
     "ASAv" : asav,
-    "NX-OSv" : n9kv
+    "NX-OSv" : n9kv,
+    "server" : linux
 }
 
 def factory_vr(name, type, image, interfaces, num):
@@ -18,6 +21,8 @@ def factory_vr(name, type, image, interfaces, num):
         return vrouters[type](name, 2048, image, interfaces, 'virtio-net-pci', num)
     elif type == "NX-OSv":
         return vrouters[type](name, 8192, image, interfaces, 'vmxnet3', num)
+    elif type == "server":
+        return vrouters[type](name, 4096, image, interfaces, 'virtio-net-pci', num)
     else:
         print("Node type not support")
         return None
